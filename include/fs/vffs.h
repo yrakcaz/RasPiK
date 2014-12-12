6fe350fc09663094fda4d3a1b12e2be30f06fa4f
Zackary Ayoun <ayoun_z@epita.fr>
2014-12-12 16:48:13 +0100

    Virtual File FS written but to be tested, VFS to be coded!

---
+++ include/fs/vffs.h
@@ -0,0 +1,38 @@
+#ifndef VFFS_H
+# define VFFS_H
+
+# include "mem.h"
+
+# define NBMAX_FILE 4096
+
+# define EOF        0xFF
+
+# define PERM_READ  0x0F
+# define PERM_WRITE 0xF0
+
+typedef struct vfile
+{
+    const char *name;
+    uint32_t size;
+    char *data;
+    int perm;
+    int lock;
+} s_vfile;
+
+typedef struct vfilefs
+{
+    s_vfile *list[NBMAX_FILE];
+    int nbfile;
+} s_vffs;
+
+int create_vfile(s_vffs *vffs, const char *name);
+s_vfile *getnode_vffs(s_vffs *vffs, const char *name);
+int remove_vfile(s_vffs *vffs, const char *name);
+int read_vfile(s_vfile *file, uint32_t offset, void *buf, uint32_t len);
+int write_vfile(s_vfile *file, uint32_t offset, const void *buf, uint32_t len);
+int chmod_vfile(s_vffs *vffs, const char *name, int perm);
+const char **readdir_vffs(s_vffs *vffs);
+s_vffs *create_vffs(void);
+int remove_vffs(s_vffs *vffs);
+
+#endif /* !VFFS_H */
--- include/fs/vfs.h
+++ include/fs/vfs.h
@@ -1,6 +1,8 @@
 #ifndef VFS_H
 # define VFS_H
 
+# include <stdint.h>
+
 // The idea is to have the most simple vfs as possible,
 // which could handle different fs types, and read/write
 // just files in it.
@@ -10,6 +12,7 @@
 // So every paths will be like that : "/<dir>/<file>".
 
 # define NBMAX_MOUNTINGPOINT 4096
+# define NBMAX_FD            4096
 
 # define DEVICES             0
 # define VFILES              1
@@ -23,6 +26,38 @@ typedef struct mounting_point
     void *fs;
 } s_mpoint;
 
-s_mpoint vfs[NBMAX_MOUNTINGPOINT];
+typedef struct vfs
+{
+    s_mpoint list[NBMAX_MOUNTINGPOINT];
+    uint32_t nbmpoints;
+} s_vfs;
+
+s_vfs vfsroot;
+
+typedef struct fd
+{
+    int type;
+    void *addr;
+    uint32_t offset;
+} s_fd;
+
+int open(const char *path, int mode);
+int close(int fd);
+
+int read(int fd, void *buf, uint32_t len);
+int write(int fd, const void *buf, uint32_t len);
+
+int remove(const char *path);
+char **readdir(const char *path);
+
+int seek(int fd, uint32_t offset, int whence);
+int ioctl(int fd, int cmd, int flags);
+
+int mount(/* DEVICE */void *dev, int type);
+int unmount(/* DEVICE */void *dev, int type);
+
+int chmod(const char *path, int mode);
+
+int init_vfs(void);
 
 #endif /* !VFS_H */
